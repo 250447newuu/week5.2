984e9d941dc7541eeb7e3c9bfa66ee8edf71830c
Asrorbek <250447@newuu.uz>
2026-02-25 10:43:50 +0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        for (Day d : Day.values()) {
+            System.out.println(d);
+        }
+    }
+}
---
+++ removeDuplicates.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class removeDuplicates {
+    private String string;
+    removeDuplicates(){
+        this.string="aabbcc";
+    }removeDuplicates(String string){
+        setString(string);
+    }public void setString(String string){
+        if(string!=null){
+            this.string=string;
+        }else{
+            System.out.println("Enter a string!!!");
+        }
+    }
+    String showResult() {
+        for (int i = 0; i < string.length(); i++) {
+            for (int j = 0; j < string.length(); j++) {
+                }
+            }
+        }
+    }
+}
